9a66b2e1b9e5a85df167b57d61a33fabe5d85eb9
Liu Xue Yan <564168+tanbro@users.noreply.github.com>
2022-06-22 11:58:43 +0800

    adjust deploy scripts

--- client/scripts/build-serverless.sh
+++ client/scripts/build-serverless.sh
@@ -6,7 +6,7 @@ export SITE_PATH=demo/trtc/web
 rm -rf dist/$SITE_PATH/*
 
 mkdir -p dist/$SITE_PATH/app
-cp -av css data img js dist/$SITE_PATH/app
+cp -av css data img js favicon.* dist/$SITE_PATH/app
 cp -av env.production.js dist/$SITE_PATH/app/env.js
 
 npm run docs:build
